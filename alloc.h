5c4026297c36bc434ad2b0c793acc51aa51d8879
18980406207@163.com <18980406207@163.com>
2021-03-05 13:03:13 +0800

    modify secondLevelAlloc feature

--- alloc.h
+++ alloc.h
@@ -68,7 +68,10 @@ void *firstLevelAlloc::oom_realloc_handler(void *pointer, size_t n) {
 
     for (;;) {
         my_oom_handler = oom_malloc_selfHandler;
-        if (nullptr == my_oom_handler) throw std::bad_alloc;
+        if (nullptr == my_oom_handler) {
+            fprintf(stderr, "out of memory\n");
+            exit(1);
+        }
         (*my_oom_handler)();
         result = realloc(pointer, n);
         if (result) return result;
@@ -162,5 +165,101 @@ void secondLevelAlloc::deallocate(void *pointer, size_t n) {
     temp->next_link = *myFreeList;
     *myFreeList = temp;
 }
+
+void *secondLevelAlloc::refill(size_t n) {
+    int nobj = 20;
+    char *chuck = chunk_alloc(n, nobj);
+    if (1 == nobj) return chuck;
+
+    obj *volatile *myFreeList = freeList + freeListIndex(n);
+    obj *result = (obj *) chuck;
+    obj *pre, *next;
+
+    //myFreeList is empty, connect to new list directly.
+    *myFreeList = next = (obj *) (chuck + n);
+    for (int i = 2; i < nobj; i++) {
+        pre = next;
+        next = (obj *) (chuck + n * i);
+        pre->next_link = next;
+    }
+    next->next_link = nullptr;
+
+    return result;
+}
+
+char *secondLevelAlloc::chunk_alloc(size_t size, int &nobjs) {
+    char *result = nullptr;
+    size_t totalBytes = size * nobjs;
+    size_t leftBytes = end_free - start_free;
+
+    if (leftBytes >= totalBytes) {
+        result = start_free;
+        start_free += totalBytes;
+        return result;
+    } else if (leftBytes >= size) {
+        nobjs = leftBytes / size;
+        result = start_free;
+        start_free += (nobjs * size);
+        return result;
+    } else {
+        //get extra space to lower time cost
+        size_t bytesToGet = 2 * totalBytes + chuckRoundUp(heap_size >> 4);
+        if (leftBytes > 0) {
+            obj *volatile *myFreeList = freeList + freeListIndex(leftBytes);
+            ((obj *) start_free)->next_link = *myFreeList;
+            *myFreeList = (obj *) start_free;
+        }
+
+        start_free = (char *) malloc(bytesToGet);
+        if (nullptr == start_free) {
+            for (int i = size; i <= MAX_BYTES; i += ALIGN) {
+                obj *volatile *myFreeList = freeList + freeListIndex(i);
+                if (nullptr != *myFreeList) {
+                    obj *temp = *myFreeList;
+                    *myFreeList = temp->next_link;
+                    start_free = (char *) temp;
+                    end_free = start_free + i;
+                    //invoke self to modify nobjs number
+                    return chunk_alloc(size, nobjs);
+                }
+            }
+            // can't find one useful freeListNode,invoke firstLevelAlloc to try
+            // firstLevelAlloc can invoke oom_handler to free space
+            // if oom still occur after oom_handler invoked ,firstLevelAlloc will throw error
+            end_free = 0;
+            start_free = (char *) malloc_alloc::allocate(bytesToGet);
+        }
+
+        heap_size += bytesToGet;
+        end_free = start_free + bytesToGet;
+        return (chunk_alloc(size, nobjs));
+    }
+}
+
+typedef secondLevelAlloc freeList_alloc;
+
+
+template<class T, class Alloc>
+class simpleAlloc {
+public:
+    static T *allocate(size_t n) {
+        return 0 == n ? 0 : (T *) Alloc::allocate(n * sizeof(T));
+    }
+
+    static T *allocate() {
+        return (T *) Alloc::allocate(sizeof(T));
+    }
+
+    static void deallocate(T *pointer, size_t n) {
+        if (0 != n) {
+            Alloc::deallocate(pointer, n);
+        }
+    }
+
+    static void deallocate(T *pointer) {
+        Alloc::deallocate(pointer, sizeof(T));
+    }
+};
+
 
 #endif //BETHSTL_ALLOC_H
